4a70ebaba38dfe4e1fd812853d67d39c7975003f
ajhamwood <ajh@tuta.io>
2018-06-03 21:41:59 +1000

    Added prose, cleaned minor UI bugs

--- js/logo.js
+++ js/logo.js
@@ -1,32 +1,32 @@
 var Logo = function () {
-  var logo = $(".logo-dynamic")[0], hcontext = logo.getContext("2d"),
+  var logo = $(".logo-dynamic")[0], logocon = logo.getContext("2d"),
       buffer1 = document.createElement("canvas"), bufcon1 = buffer1.getContext("2d"),
       buffer2 = document.createElement("canvas"), bufcon2 = buffer2.getContext("2d"),
       vw, vh, loop, count = 0;
 
 // Render loop
   var Logo = Object.create({
-    loop, loopGen () {
+    resize, loop, loopGen () {
       let frameTiming = 1000/15;
       bufcon2.setTransform(-1, 0, 0, 1, 0, 0);
-      hcontext.fillStyle = "#000";
+      logocon.fillStyle = "#000";
       return () => {
         performance.mark('startloop');
         updateTransform();
         let hvw = Math.floor(vw/2);
-        hcontext.fillRect(0, 0, vw, vh);
-        hcontext.transform.apply(hcontext, transform.get());
-        hcontext.filter = "url(#glow)";
-        hcontext.drawImage(buffer1, 0, 0, vw, vh);
-        hcontext.filter = "none";
+        logocon.fillRect(0, 0, vw, vh);
+        logocon.transform.apply(logocon, transform.get());
+        logocon.filter = "url(#glow)";
+        logocon.drawImage(buffer1, 0, 0, vw, vh);
+        logocon.filter = "none";
         bufcon1.drawImage(logo, 0, 0, hvw, vh, 0, 0, hvw, vh);
         bufcon2.drawImage(logo, 0, 0, hvw, vh, 0, 0, -hvw, vh);
         bufcon1.drawImage(buffer2, 0, 0, hvw, vh, hvw, 0, hvw, vh);
-        hcontext.resetTransform();
-        hcontext.drawImage(buffer1, 0, 0, vw, vh);
+        logocon.resetTransform();
+        logocon.drawImage(buffer1, 0, 0, vw, vh);
         performance.mark('endloop');
         performance.measure('looptime', 'startloop', 'endloop');
-        setTimeout(Logo.loop, Math.max(frameTiming - performance.getEntriesByName("looptime")[0].duration, 0));
+        setTimeout(this.loop, Math.max(frameTiming - performance.getEntriesByName("looptime")[0].duration, 0));
         performance.clearMarks();
         performance.clearMeasures()
       }
@@ -42,7 +42,7 @@ var Logo = function () {
   }
 
 // Transformer object
-  function T (vw, vh) {}
+  function T () {}
 
   T.prototype = {
     get: function () { return this._t },
@@ -58,19 +58,24 @@ var Logo = function () {
     constructor: T
   };
 
-  var transform;
+  var transform = new T();
 
-  var img1 = new Image(logo.width, logo.height);
-  img1.src = logo.toDataURL();
-  var img2 = new Image(buffer1.width, buffer1.height);
-  img2.src = buffer1.toDataURL();
-  var img3 = new Image(buffer2.width, buffer2.height);
-  img3.src = buffer2.toDataURL();
-  logo.width = buffer1.width = buffer2.width = vw = logo.clientWidth * 2;
-  logo.height = buffer1.height = buffer2.height = vh = logo.clientHeight * 2;
-  return Promise.all([
-    new Promise(resolve => img1.onload = () => resolve(hcontext.drawImage(img1, 0, 0))),
-    new Promise(resolve => img2.onload = () => resolve(bufcon1.drawImage(img2, 0, 0))),
-    new Promise(resolve => img3.onload = () => resolve(bufcon2.drawImage(img3, 0, 0)))
-  ]).then(() => transform = new T(vw, vh)).then(() => Logo);
+  function resize (w, h) {
+    'loop' in this && (this.loop = () => {});
+    var img1 = new Image(logo.width, logo.height);
+    img1.src = logo.toDataURL();
+    var img2 = new Image(buffer1.width, buffer1.height);
+    img2.src = buffer1.toDataURL();
+    var img3 = new Image(buffer2.width, buffer2.height);
+    img3.src = buffer2.toDataURL();
+    logo.width = buffer1.width = buffer2.width = vw = (w || logo.clientWidth) * 2;
+    logo.height = buffer1.height = buffer2.height = vh = (h || logo.clientHeight) * 2;
+    return Promise.all([
+      new Promise(resolve => img1.onload = () => resolve(logocon.drawImage(img1, 0, 0, vw, vh))),
+      new Promise(resolve => img2.onload = () => resolve(bufcon1.drawImage(img2, 0, 0, vw, vh))),
+      new Promise(resolve => img3.onload = () => resolve(bufcon2.drawImage(img3, 0, 0, vw, vh)))
+    ]).then(() => 'loop' in this && (this.loop = this.loopGen())())
+  }
+
+  return resize().then(() => Logo);
 }
